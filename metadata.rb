6391057a3c271148f9e2a776b6b54cdc21757335
Samuel Cassiba <s@cassiba.com>
2018-04-21 19:09:22 -0700

    Update metadata for migration to OpenStack

--- metadata.rb
+++ metadata.rb
@@ -1,26 +1,10 @@
-#  encoding: UTF-8
-#
-#  Copyright 2016 cloudbau GmbH
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
-
-name 'openstackclient'
-maintainer 'cloudbau GmbH'
-maintainer_email 'j.klare@cloudbau.de'
+name 'openstack-client'
+maintainer 'openstack-chef'
+maintainer_email 'openstack-dev@lists.openstack.org'
 license 'Apache-2.0'
 description 'Installs the fog-openstack gem and offers LWRPs to use it'
-issues_url 'https://github.com/cloudbau/cookbook-openstackclient/issues'
-source_url 'https://github.com/cloudbau/cookbook-openstackclient'
+issues_url 'https://launchpad.net/openstack-chef'
+source_url 'https://git.openstack.org/openstack/cookbook-openstack-client'
 chef_version '>= 12.5' if respond_to?(:chef_version)
 version '17.0.0'
 
