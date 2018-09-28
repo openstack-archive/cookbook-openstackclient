d3ac8043f512826ebf01b4203a0825dc7076863e
Roger Luethi <rl@patchworkscience.org>
2018-09-28 08:15:43 +0200

    Pin fog-openstack to 0.3.4
    With 0.3.5, fog-openstack changes the way it handles versioned identity
    endpoints, resulting in errors such as:
    
      Excon::Error::NotFound: openstack_domain[default]
      (openstack-identity::registration line 69) had an error:
      Excon::Error::NotFound: Expected([200, 201]) <=> Actual(404 Not Found)
      excon.error.response
        :body          => "{\"error\": {\"message\":
        \"(http://127.0.0.1:35357/v3/auth/tokens/v3/auth/tokens): The resource
        could not be found.\", \"code\": 404, \"title\": \"Not Found\"}}"
        :cookies       => [
        ]
        :headers       => {
          "Content-Length"         => "149"
          "Content-Type"           => "application/json"
          "Date"                   => "Fri, 28 Sep 2018 06:27:48 GMT"
          "Server"                 => "Apache"
          "Vary"                   => "X-Auth-Token"
          "X-Distribution"         => "Ubuntu"
          "x-openstack-request-id" => "req-bc3aed4d-a4f2-4224-8aef-c675387d3a27"
        }
        :host          => "127.0.0.1"
        :local_address => "127.0.0.1"
        :local_port    => 42372
        :path          => "/v3/auth/tokens/v3/auth/tokens"
        :port          => 35357
        :reason_phrase => "Not Found"
        :remote_ip     => "127.0.0.1"
        :status        => 404
        :status_line   => "HTTP/1.1 404 Not Found\r\n"
    
    Change-Id: Icfb0cd289ed62e7433cc1ee2341a70d649fc6aa0
    (cherry picked from commit 876cb832c2d431b112c4e6b20f96ffe7b1cb735e)

--- metadata.rb
+++ metadata.rb
@@ -6,10 +6,10 @@ description 'Installs the fog-openstack gem and offers LWRPs to use it'
 issues_url 'https://launchpad.net/openstack-chef'
 source_url 'https://git.openstack.org/openstack/cookbook-openstack-client'
 chef_version '>= 12.5' if respond_to?(:chef_version)
-version '17.0.3'
+version '17.0.4'
 
 %w(ubuntu redhat centos).each do |os|
   supports os
 end
 
-gem 'fog-openstack', '< 1.0.0'
+gem 'fog-openstack', '= 0.3.4'
