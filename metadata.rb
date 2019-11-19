26826bfa8e714b506e2c210e551ad78a95d98125
inspurericzhang <zhanglf01@inspur.com>
2019-11-19 15:24:31 +0800

    Replace git.openstack.org with opendev.org
    Change-Id: I247616defa3244d9bda54757659260d8b95d0005

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'openstack-discuss@lists.openstack.org'
 license 'Apache-2.0'
 description 'Installs the fog-openstack gem and offers LWRPs to use it'
 issues_url 'https://launchpad.net/openstack-chef'
-source_url 'https://git.openstack.org/openstack/cookbook-openstack-client'
+source_url 'https://opendev.org/openstack/cookbook-openstack-client'
 chef_version '>= 12.5' if respond_to?(:chef_version)
 version '18.0.0'
 
